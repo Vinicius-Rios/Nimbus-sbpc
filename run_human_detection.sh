00cab659bbbef769a31ce6846dc0ee3c87c31f11
vinicius-rios <vinicius.rios40@gmail.com>
2024-07-06 17:14:12 -0300

    shell added to run and open image

---
+++ run_human_detection.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Open the image
+xdg-open /home/sbpc-2024/Downloads/Nimbus-sbpc/temp/images_edited/image_with_count_and_boxes.png &
+
+# Execute the Python script
+python3 /home/sbpc-2024/Downloads/Nimbus-sbpc/Human_Detection/human_detection.py
